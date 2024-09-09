b84e156806a351595d7dde224184af10b02992a8
Vagarth <vagarth419@gmail.com>
2024-09-09 13:27:44 +0530

    feat: complete in memory todo app

--- http-todo-application/index.js
+++ http-todo-application/index.js
@@ -1,11 +1,29 @@
+//IN MEMORY TODO APP
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-const todo = ["Code", "Coffee"];
+let todo = ["Code", "Coffee"];
 
-app.get('/', (req, res) => [
+app.get('/', (req, res) => {
     res.send(todo)
-])
+})
+
+app.post('/', (req, res) => {
+    todo.push(req.body.todo);
+    res.send(todo);
+})
+
+app.delete('/', (req, res) => {
+    todo = todo.filter(item => item !== req.body.todo);
+    res.send(todo);
+})
+
+app.put('/', (req, res) => {
+    const index = todo.indexOf(req.body.oldTodo);
+    todo[index] = req.body.newTodo;
+    res.send(todo);
+})
 
 app.listen(3000);
