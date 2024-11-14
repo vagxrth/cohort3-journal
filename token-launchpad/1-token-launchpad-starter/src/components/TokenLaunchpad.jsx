416ffd1e2cba1abc71fcd8f31cbd718e20bf6af7
Vagarth <vagarth419@gmail.com>
2024-11-15 00:55:33 +0530

    feat: create token creation functionality

--- token-launchpad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ token-launchpad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,5 +1,31 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { createInitializeMint2Instruction, getMinimumBalanceForRentExemptAccount, MINT_SIZE, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 export function TokenLaunchpad() {
+
+    const { connection } = useConnection();
+    const wallet = useWallet();
+
+    const createToken = async() => {
+        const mintKeypair = Keypair.generate();
+        const lamports = await getMinimumBalanceForRentExemptAccount(connection);
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: mintKeypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId: TOKEN_PROGRAM_ID,
+            }),
+            createInitializeMint2Instruction(mintKeypair.publicKey, 9, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID)
+        );
+
+        transaction.partialSign(mintKeypair);
+        await wallet.signTransaction(transaction);
+    }
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -12,6 +38,6 @@ export function TokenLaunchpad() {
         <input className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input className='inputText' type='text' placeholder='Image URL'></input> <br />
         <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <button className='btn' onClick={createToken}>Create a token</button>
     </div>
 }
