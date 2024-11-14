e4ba3e6b66e43a1d0e64425b63c8a1b58fbef10f
Vagarth <vagarth419@gmail.com>
2024-11-14 14:19:59 +0530

    feat: add transaction functionality

--- eth-wallet-adapter/src/App.jsx
+++ eth-wallet-adapter/src/App.jsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
-import { createConfig, http, useAccount, useBalance, useConnect, useDisconnect, WagmiProvider } from 'wagmi'
+import { createConfig, http, useAccount, useBalance, useConnect, useDisconnect, useSendTransaction, WagmiProvider } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
 import { injected, metaMask, safe } from 'wagmi/connectors'
 
@@ -55,12 +55,30 @@ function App() {
     )
   }
 
+  const SendETH = () => {
+    const { data: hash, sendTransaction }  = useSendTransaction();
+
+    const sendTxn = async() => {
+      const to = document.getElementById('to').value;
+      sendTransaction({ to, value: '100000000000000000'});
+    }
+
+    return (
+      <div>
+        <input type="text" id='to' placeholder='To Address' />
+        <button onClick={sendTxn}>Send 0.1 ETH</button>
+        { hash && <div>Transaction Hash: { hash }</div> }
+      </div>
+    )
+  }
+
   return (
     <>
       <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
           <WalletConnector />
           <Balance />
+          <SendETH />
         </QueryClientProvider>
       </WagmiProvider>
     </>
