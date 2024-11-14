fbca4c644473a3f32a843658dcc9156ed3249ad9
Vagarth <vagarth419@gmail.com>
2024-11-14 13:57:15 +0530

    feat: add wallet connect

--- eth-wallet-adapter/src/App.jsx
+++ eth-wallet-adapter/src/App.jsx
@@ -1,5 +1,6 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
-import { createConfig, http } from 'wagmi'
+import { createConfig, http, useConnect, WagmiProvider } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
 import { injected, metaMask, safe } from 'wagmi/connectors'
 
@@ -15,12 +16,30 @@ const config = createConfig({
   }
 })
 
+const queryClient = new QueryClient()
+
 function App() {
+
+  const WalletConnector = () => {
+    const { connectors, connect } = useConnect();
+
+    return (
+      connectors.map((connector) => (
+        <button key={connector.id} onClick={() => connect({ connector })}>
+          {connector.name}
+          {connector.error && <div>(error: {connector.error.message})</div>}
+        </button>
+      ))
+    )
+  }
 
   return (
     <>
-      <input type="text" placeholder='Address' />
-      <button>Send 0.1 ETH</button>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <WalletConnector />
+        </QueryClientProvider>
+      </WagmiProvider>
     </>
   )
 }
