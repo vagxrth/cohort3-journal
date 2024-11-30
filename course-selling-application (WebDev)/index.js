b96036743658ac7f33057a1587d0310baa1a5e1c
Vagarth <vagarth419@gmail.com>
2024-11-30 16:36:33 +0530

    feat: add skeleton routes for signin, signup and course purchase

---
+++ course-selling-application (WebDev)/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+
+app.post("/signup", (req, res) => {
+    
+})
+
+app.post("/signin", (req, res) => {
+
+})
+
+app.get("/courses", (req, res) => {
+
+})
+
+app.get("/purchases", (req, res) => {
+
+})
+
+app.get("/buy", (req, res) => {
+    
+})
+
+app.listen(3000);
