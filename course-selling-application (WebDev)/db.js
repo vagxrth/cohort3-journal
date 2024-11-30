9c440ab374ca43a025c5772a0c0e512163b650b2
Vagarth <vagarth419@gmail.com>
2024-11-30 18:12:04 +0530

    feat: add a schema for purchases

--- course-selling-application (WebDev)/db.js
+++ course-selling-application (WebDev)/db.js
@@ -18,5 +18,22 @@ const adminSchema = new Schema({
     password: String
 })
 
+const courseSchema = new Schema({
+    _id: ObjectId,
+    title: String,
+    description: String,
+    price: Number,
+    imageURL: String,
+    creator: { type: ObjectId, ref: 'admin'}
+})
+
+const purchaseSchema = new Schema({
+    _id: ObjectId,
+    course: { type: ObjectId, ref: 'course'},
+    user: { type: ObjectId, ref: 'user'}
+})
+
 export const UserModel = mongoose.model('user', userSchema);
 export const AdminModel = mongoose.model('admin', adminSchema);
+export const CourseModel = mongoose.model('course', courseSchema);
+export const PurchaseModel = mongoose.model('purchase', purchaseSchema);
