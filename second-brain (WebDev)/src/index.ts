4be7333415c3250d3a54103cb5caf6d900174527
Vagarth <vagarth419@gmail.com>
2025-01-24 08:13:14 +0530

    feat: create express routes

---
+++ second-brain (WebDev)/src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+
+const app = express();
+
+app.post('/signup', (req, res) => {
+
+})
+
+app.post('/signin', (req, res) => {
+
+})
+
+app.post('/add-content', (req, res) => {
+
+})
+
+app.get('/your-content', (req, res) => {
+
+})
+
+app.delete('/delete-content', (req, res) => {
+
+})
+
+app.post('/share', (req, res) => {
+
+})
+
+app.get('/share/:userId', (req, res) => {
+    
+})
