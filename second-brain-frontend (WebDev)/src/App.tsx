99c4a0badbe918b5b76703acc95207b25d48fea2
Vagarth <vagarth419@gmail.com>
2025-02-01 04:29:09 +0530

    ui: add share icon

--- second-brain-frontend (WebDev)/src/App.tsx
+++ second-brain-frontend (WebDev)/src/App.tsx
@@ -1,12 +1,13 @@
 import Button from './components/Button'
 import Plus from '../src/components/icons/Plus'
+import Share from './components/icons/Share'
 
 function App() {
 
   return (
     <>
-      <Button variant='primary' size='sm' text='Add Content' onClick={() => {}} icon={<Plus size='lg' />}/>
-      <Button variant='secondary' size='sm' text='Share' onClick={() => {}}/>
+      <Button variant='primary' size='sm' text='Add Content' onClick={() => {}} icon={<Plus size='md' />}/>
+      <Button variant='secondary' size='sm' text='Share' onClick={() => {}} icon={<Share size='md'/>}/>
     </>
   )
 }
