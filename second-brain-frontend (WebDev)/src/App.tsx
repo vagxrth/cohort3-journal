906ec6afc857d1e27f294b1cd9b726fea4fd555f
Vagarth <vagarth419@gmail.com>
2025-02-04 22:52:53 +0530

    feat: rendering sidebar and content page in app

--- second-brain-frontend (WebDev)/src/App.tsx
+++ second-brain-frontend (WebDev)/src/App.tsx
@@ -1,16 +1,12 @@
-import { PlusCircle, Share2 } from 'lucide-react'
-import { Button } from './components/Button'
+import { ContentPage } from "./components/ContentPage"
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
 
   return (
-    <div className='flex'>
-      <Button variant="secondary" icon={Share2}>
-        Share Brain
-      </Button>
-      <Button variant="primary" icon={PlusCircle}>
-        Add Content
-      </Button>
+    <div className="flex min-h-screen bg-gray-50">
+      <Sidebar />
+      <ContentPage />
     </div>
   )
 }
