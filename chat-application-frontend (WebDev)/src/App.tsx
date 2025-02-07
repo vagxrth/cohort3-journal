ccf52cda037ab791f573111d79304836e8fe262f
Vagarth <vagarth419@gmail.com>
2025-02-07 10:25:23 +0530

    feat: integrate WebSocket connection in App component for real-time messaging

--- chat-application-frontend (WebDev)/src/App.tsx
+++ chat-application-frontend (WebDev)/src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [socket, setSocket] = useState<WebSocket | null>();
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080')
+    ws.onopen= () => {
+      console.log('Connection established');
+      ws.send("Hello From Client!")
+    }
+    ws.onmessage = (message) => {
+      console.log('Message received:', message.data);
+
+      setSocket(ws)
+
+      return () => ws.close();
+    }
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" placeholder='Enter Your Message' />
     </>
   )
 }
