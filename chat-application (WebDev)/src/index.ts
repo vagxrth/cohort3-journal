c424e723b5228338f04e3e21136affbd074589a6
Vagarth <vagarth419@gmail.com>
2025-02-07 10:07:27 +0530

    feat: enhance WebSocket server to broadcast messages to all connected users

--- chat-application (WebDev)/src/index.ts
+++ chat-application (WebDev)/src/index.ts
@@ -1,10 +1,19 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const ws = new WebSocketServer({ port: 8080 })
 
+let userCount = 0;
+const allSockets: WebSocket[] = []
+
 ws.on('connection', (socket) => {
+
+    allSockets.push(socket)
+
+    userCount++;
+    console.log(`User Connected: ${userCount}`)
+
     socket.on('message', (data) => {
         console.log('Received Data: %s', data)
+        allSockets.forEach((s) => s.send(`Server Send This Message: ${data}`))
     })
-    socket.send('Hello from server!')
 })
