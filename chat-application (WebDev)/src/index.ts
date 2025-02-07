e797058f6ce8239b775ca98b5569d2064e0c0d6a
Vagarth <vagarth419@gmail.com>
2025-02-07 09:27:10 +0530

    feat: implement WebSocket server for real-time communication

--- chat-application (WebDev)/src/index.ts
+++ chat-application (WebDev)/src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello")
+import { WebSocketServer } from "ws";
+
+const ws = new WebSocketServer({ port: 8080 })
+
+ws.on('connection', (socket) => {
+    socket.on('message', (data) => {
+        console.log('Received Data: %s', data)
+    })
+    socket.send('Hello from server!')
+})
